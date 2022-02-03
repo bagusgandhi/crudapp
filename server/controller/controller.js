75edda42443d949c76c2bab853e0009f83a12e79
bagusgandhi <bagusgandhi4@gmail.com>
2022-02-03 19:20:27 +0700

    create api crud

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -38,10 +38,47 @@ exports.find = (req, res) => {
 
 //update user
 exports.update = (req, res) => {
+    if(!req.body){
+        return res.status(400).send({
+            message:"Data to update cannot be empty" 
+        })
+    }
 
+    // params :id
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id, req.body, {useFindAndModify: false}).then(
+        data => {
+            if(!data){
+                res.status(404).send({message:`Cannot Update user with ${id}.`})
+            }else{
+                res.send(data)
+
+            }
+        }
+    ).catch(err => {
+        res.status(500).send({
+            message:"Error Update user information"
+        })
+    });
 }
 
 //delete user
 exports.delete = (req, res) => {
+    // params :id
+    const id = req.params.id;
 
+    Userdb.findByIdAndDelete(id)
+    .then(data => {
+        if(!data){
+            res.status(400).send({message:`Cannot Delete user with id ${id}`})
+        }else{
+            res.send({
+                message:"User was deleted successfully"
+            })
+        }
+    }).catch(err => {
+        res.status(500).send({
+            message:"could not delete user with id "+id
+        })
+    });
 }
